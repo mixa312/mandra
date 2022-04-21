be2ca5f791fc05f32570675d608722fc7636093a
Mikhail Mandra <mandra.mb@phystech.edu>
2022-04-21 15:28:25 +0300

    7

---
+++ 7-1.py
@@ -0,0 +1,83 @@
+import RPi.GPIO as GPIO
+import time
+import matplotlib.pyplot as plt
+
+dac = [26, 19, 13, 6, 5, 11, 9, 10]
+leds = [21, 20, 16, 12, 7, 8, 25, 24]
+comp = 4
+troyka = 17
+
+GPIO.setwarnings(False) 
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
+GPIO.setup(leds, GPIO.OUT)
+GPIO.setup(troyka, GPIO.OUT)
+GPIO.setup(comp, GPIO.IN)
+
+
+def decimal2binary(value):
+    return[int(element) for element in bin(value)[2:].zfill(8)]
+
+def adc():
+    value = 0
+    for i in range(8):
+        GPIO.output(dac[i], 1)
+        time.sleep(0.007)
+        
+        if (GPIO.input(comp) == 0):
+            GPIO.output(dac[i], 0)
+
+        else:
+            value += (1<<(7-i))
+    GPIO.output(dac, 0)
+    return(value)
+        
+
+def show_voltage(value):
+    GPIO.output(leds, decimal2binary(value))
+
+
+try:
+    voltage_results = []
+
+    start_time = time.time()
+    GPIO.output(troyka, 1)
+    current_voltage = adc()
+    while current_voltage < 0.97*256:
+        show_voltage(current_voltage)
+        print(current_voltage, "{:.2f}V".format(current_voltage/256*3.3))
+        voltage_results.append(current_voltage/256*3.3)
+        current_voltage = adc()
+
+    current_voltage = adc()
+    GPIO.output(troyka, 0)
+    while current_voltage > 0.02*256:
+        show_voltage(current_voltage)
+        print(current_voltage, "{:.2f}V".format(current_voltage/256*3.3))
+        voltage_results.append(current_voltage/256*3.3)
+        current_voltage = adc()
+    end_time = time.time()
+
+    experiment_time = end_time - start_time
+
+    plt.plot(voltage_results)
+    plt.show()
+
+
+    with open("data.txt", 'w') as data:
+        data.write('\n'.join([str(item) for item in voltage_results]))
+    with open("settings.txt", 'w') as settings:
+        settings.write("Средняя частота дискретизации: {:.2f} /n ".format(len(voltage_results)/experiment_time))
+        settings.write("Шаг квантования АЦП: {:.2f}V".format(3.3/256))
+
+    print("Время эксперимента: ", str(experiment_time))
+    print("Период одного измерения: {:.2f}".format(experiment_time/len(voltage_results)))
+    print("Средняя частота дискретизации: {:.2f}".format(len(voltage_results)/experiment_time))
+    print("шаг квантования АЦП: {:.2f}V".format(3.3/256))
+
+finally:
+    GPIO.output(dac, 0)
+    GPIO.output(leds, 0)
+    GPIO.output(troyka, 0)
+    GPIO.cleanup()
